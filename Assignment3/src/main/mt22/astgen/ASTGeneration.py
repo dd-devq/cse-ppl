8ee86791d888944cb96e89dfd88b94a4c382fdc8
dd-devq <dd.devq@gmail.com>
2023-03-21 08:21:36 +0700

    Cleanup & Add Assignment 3

---
+++ Assignment3/src/main/mt22/astgen/ASTGeneration.py
@@ -0,0 +1,249 @@
+from MT22Visitor import MT22Visitor
+from MT22Parser import MT22Parser
+from AST import *
+
+
+class ASTGeneration(MT22Visitor):
+    def visitProgram(self, ctx: MT22Parser.ProgramContext):
+        decls = []
+        for decl in ctx.decl():
+            decls.extend(self.visit(decl))
+        return Program(decls)
+
+    def visitDecl(self, ctx: MT22Parser.DeclContext):
+        return self.visit(ctx.getChild(0))
+
+    def visitVardecl(self, ctx: MT22Parser.VardeclContext):
+        return self.visit(ctx.getChild(0))
+
+    def visitFuncdecl(self, ctx: MT22Parser.FuncdeclContext):
+        inherit = None
+        id = ctx.ID().getText()
+        if ctx.funcinherit():
+            inherit = self.visit(ctx.funcinherit())
+        return [FuncDecl(id, self.visit(ctx.functype()), self.visit(ctx.paramlist()), inherit, self.visit(ctx.body()))]
+
+    def visitFunctype(self, ctx: MT22Parser.FunctypeContext):
+        return self.visit(ctx.primitivetype())
+
+    def visitFuncinherit(self, ctx: MT22Parser.FuncinheritContext):
+        return ctx.ID().getText()
+
+    def visitBody(self, ctx: MT22Parser.BodyContext):
+        return self.visit(ctx.blockstmt())
+
+    def visitParamlist(self, ctx: MT22Parser.ParamlistContext):
+        return [self.visit(param) for param in ctx.param()]
+
+    def visitParam(self, ctx: MT22Parser.ParamContext):
+        out = ctx.OUT()
+        inherit = ctx.INHERIT()
+        return ParamDecl(ctx.ID().getText(), self.visit(ctx.primitivetype()), out, inherit)
+
+    def visitNoassign(self, ctx: MT22Parser.NoassignContext):
+        idlst = self.visit(ctx.idlst())
+        primitivetype = self.visit(ctx.primitivetype())
+        return [VarDecl(str(id), primitivetype) for id in idlst]
+
+    def visitMultipleassign(self, ctx: MT22Parser.MultipleassignContext):
+        id = [ctx.ID().getText()]
+        extid, primitivetype, extexpr = self.visit(ctx.extassign())
+        expr = [self.visit(ctx.expr())]
+        id.extend(extid)
+        expr.extend(extexpr)
+        list(expr)
+        expr.reverse()
+        return [VarDecl(str(id), primitivetype, expr) for id, expr in zip(id, expr)]
+
+    def visitExtassign(self, ctx: MT22Parser.ExtassignContext):
+        if ctx.extassign():
+            id, expr = [ctx.ID().getText()], [self.visit(ctx.expr())]
+            primitivetype = None
+            extid, primitivetype, extexpr = self.visit(ctx.extassign())
+            id += extid
+            expr += extexpr
+            return id, primitivetype, expr
+        else:
+            primitivetype = self.visit(ctx.primitivetype())
+            return [], primitivetype, []
+
+    def visitPrimitivetype(self, ctx: MT22Parser.PrimitivetypeContext):
+        if ctx.INTEGER():
+            return IntegerType()
+        elif ctx.FLOAT():
+            return FloatType()
+        elif ctx.STRING():
+            return StringType()
+        elif ctx.BOOLEAN():
+            return BooleanType()
+        elif ctx.VOID():
+            return VoidType()
+        elif ctx.AUTO():
+            return AutoType()
+        elif ctx.arrayType():
+            return self.visit(ctx.arrayType())
+
+    def visitArrayType(self, ctx: MT22Parser.ArrayTypeContext):
+        return ArrayType(self.visit(ctx.dimension()), self.visit(ctx.primitivetype()))
+
+    def visitDimension(self, ctx: MT22Parser.DimensionContext):
+        return [index.getText() for index in ctx.INTL()]
+
+    def visitIdlst(self, ctx: MT22Parser.IdlstContext):
+        return [id.getText() for id in ctx.ID()]
+
+    def visitExprlst(self, ctx: MT22Parser.ExprlstContext):
+        return [self.visit(ex) for ex in ctx.expr()]
+
+    def visitExpr(self, ctx: MT22Parser.ExprContext):
+
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.getChild(0))
+        return BinExpr(ctx.getChild(1).getText(), self.visit(ctx.expr1(0)), self.visit(ctx.expr1(1)))
+
+    def visitExpr1(self, ctx: MT22Parser.Expr1Context):
+        if ctx.getChildCount() == 3:
+            return BinExpr(ctx.getChild(1).getText(), self.visit(ctx.expr2(0)), self.visit(ctx.expr2(1)))
+        return self.visit(ctx.getChild(0))
+
+    def visitExpr2(self, ctx: MT22Parser.Expr2Context):
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.getChild(0))
+        return BinExpr(ctx.getChild(1).getText(), self.visit(ctx.expr2()), self.visit(ctx.expr3()))
+
+    def visitExpr3(self, ctx: MT22Parser.Expr3Context):
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.getChild(0))
+        return BinExpr(ctx.getChild(1).getText(), self.visit(ctx.expr3()), self.visit(ctx.expr4()))
+
+    def visitExpr4(self, ctx: MT22Parser.Expr4Context):
+        if ctx.getChildCount() == 1:
+            return self.visit(ctx.getChild(0))
+        return BinExpr(ctx.getChild(1).getText(), self.visit(ctx.expr4()), self.visit(ctx.expr5()))
+
+    def visitExpr5(self, ctx: MT22Parser.Expr5Context):
+        if ctx.getChildCount() == 2:
+            return UnExpr(ctx.getChild(0).getText(), self.visit(ctx.getChild(1)))
+        return self.visit(ctx.getChild(0))
+
+    def visitExpr6(self, ctx: MT22Parser.Expr6Context):
+        if ctx.getChildCount() == 2:
+            return UnExpr(ctx.getChild(0).getText(), self.visit(ctx.getChild(1)))
+        return self.visit(ctx.getChild(0))
+
+    def visitExpr7(self, ctx: MT22Parser.Expr7Context):
+        if ctx.getChildCount() == 4:
+            return ArrayCell(self.visit(ctx.expr7()), self.visit(ctx.exprlst()))
+        return self.visit(ctx.getChild(0))
+
+    def visitExpr8(self, ctx: MT22Parser.Expr8Context):
+        if ctx.getChildCount() == 3:
+            return self.visit(ctx.expr())
+        return self.visit(ctx.getChild(0))
+
+    def visitOperands(self, ctx: MT22Parser.OperandsContext):
+        if ctx.INTL():
+            return IntegerLit(int(ctx.INTL().getText()))
+        elif ctx.FLOATL():
+            return FloatLit(float(ctx.FLOATL().getText()))
+        elif ctx.STRINGL():
+            return StringLit(ctx.STRINGL().getText())
+        elif ctx.BOOLL():
+            return BooleanLit(True if ctx.BOOLL().getText() == "true" else False)
+        elif ctx.ID():
+            return Id(ctx.ID().getText())
+        elif ctx.arrayL():
+            return self.visit(ctx.arrayL())
+        elif ctx.funccall():
+            return self.visit(ctx.funccall())
+
+    def visitArrayL(self, ctx: MT22Parser.ArrayLContext):
+        return ArrayLit(self.visit(ctx.exprlst()))
+
+    def visitFunccall(self, ctx: MT22Parser.FunccallContext):
+        id = ctx.ID().getText()
+        if ctx.exprlst():
+            return FuncCall(id, self.visit(ctx.exprlst()))
+        return FuncCall(id, [])
+
+    def visitStmt(self, ctx: MT22Parser.StmtContext):
+        if ctx.assignstmt():
+            return self.visit(ctx.assignstmt())
+        if ctx.vardecl():
+            return self.visit(ctx.vardecl())
+        if ctx.forstmt():
+            return self.visit(ctx.forstmt())
+        if ctx.ifstmt():
+            return self.visit(ctx.ifstmt())
+        if ctx.returnstmt():
+            return self.visit(ctx.returnstmt())
+        if ctx.callstmt():
+            return self.visit(ctx.callstmt())
+        if ctx.breakstmt():
+            return self.visit(ctx.breakstmt())
+        if ctx.continuestmt():
+            return self.visit(ctx.continuestmt())
+        if ctx.whilestmt():
+            return self.visit(ctx.whilestmt())
+        if ctx.dowhilestmt():
+            return self.visit(ctx.dowhilestmt())
+        return self.visit(ctx.blockstmt())
+
+    def visitAssignstmt(self, ctx: MT22Parser.AssignstmtContext):
+        if ctx.ID():
+            return AssignStmt(ctx.Id().getText(), self.visit(ctx.expr()))
+        return AssignStmt(self.visit(ctx.getChild(0)), self.visit(ctx.expr()))
+
+    def visitForstmt(self, ctx: MT22Parser.ForstmtContext):
+        stmt = AssignStmt(Id(ctx.ID().getText()), self.visit(ctx.expr(0)))
+
+        return ForStmt(stmt, self.visit(ctx.expr(1)), self.visit(ctx.expr(2)), self.visit(ctx.stmt()))
+
+    def visitIfstmt(self, ctx: MT22Parser.IfstmtContext):
+        if ctx.falsestmt():
+            return IfStmt(self.visit(ctx.expr()), self.visit(ctx.truestmt()), self.visit(ctx.falsestmt()))
+        else:
+            return IfStmt(self.visit(ctx.expr()), self.visit(ctx.truestmt()), None)
+
+    def visitTruestmt(self, ctx: MT22Parser.TruestmtContext):
+        return self.visit(ctx.stmt())
+
+    def visitFalsestmt(self, ctx: MT22Parser.FalsestmtContext):
+        return self.visit(ctx.stmt())
+
+    def visitReturnstmt(self, ctx: MT22Parser.ReturnstmtContext):
+        if ctx.expr():
+            return ReturnStmt(self.visit(ctx.expr()))
+        return ReturnStmt(None)
+
+    def visitCallstmt(self, ctx: MT22Parser.CallstmtContext):
+        if ctx.exprlst():
+            return CallStmt(ctx.ID().getText(), self.visit(ctx.exprlst()))
+        return CallStmt(ctx.ID().getText(), [])
+
+    def visitBreakstmt(self, ctx: MT22Parser.BreakstmtContext):
+        return BreakStmt()
+
+    def visitContinuestmt(self, ctx: MT22Parser.ContinuestmtContext):
+        return ContinueStmt()
+
+    def visitWhilestmt(self, ctx: MT22Parser.WhilestmtContext):
+        if ctx.getChildCount() == 5:
+            return WhileStmt(self.visit(ctx.expr()), self.visit(ctx.stmt(0)))
+        else:
+            return
+
+    def visitDowhilestmt(self, ctx: MT22Parser.DowhilestmtContext):
+        return DoWhileStmt(self.visit(ctx.expr()), self.visit(ctx.blockstmt()))
+
+    def visitBlockstmt(self, ctx: MT22Parser.BlockstmtContext):
+        stmts = flatten([self.visit(stmt) for stmt in ctx.stmt()])
+        return BlockStmt(stmts)
+
+
+def flatten(list_of_lists):
+    if len(list_of_lists) == 0:
+        return list_of_lists
+    if isinstance(list_of_lists[0], list):
+        return flatten(list_of_lists[0]) + flatten(list_of_lists[1:])
+    return list_of_lists[:1] + flatten(list_of_lists[1:])
---
+++ Assignment3/src/main/mt22/parser/MT22.g4
@@ -0,0 +1,219 @@
+grammar MT22;
+
+/*
+ ID: 2052904
+ */
+
+@lexer::header {
+from lexererr import *
+}
+
+options {
+	language = Python3;
+}
+
+program: decl+ EOF;
+
+decl: vardecl | funcdecl;
+
+vardecl: (noassign | multipleassign) SEMI;
+noassign: idlst COLON primitivetype;
+multipleassign: ID extassign expr;
+extassign: (COMMA ID) extassign (expr COMMA)
+	| COLON primitivetype ASSIGN;
+
+
+funcdecl: ID COLON FUNCTION functype LB paramlist RB funcinherit? body;
+funcinherit: INHERIT ID;
+paramlist: param (COMMA param)* | ;
+param: INHERIT? OUT? ID COLON primitivetype;
+body: blockstmt;
+
+stmt:
+	assignstmt
+	| vardecl
+	| forstmt
+	| ifstmt
+	| returnstmt
+	| callstmt
+	| breakstmt
+	| continuestmt
+	| whilestmt
+	| dowhilestmt
+	| blockstmt;
+
+assignstmt: (expr7 | ID) ASSIGN expr SEMI;
+
+breakstmt: BREAK SEMI;
+continuestmt: CONTINUE SEMI;
+ifstmt: IF LB expr RB truestmt (ELSE falsestmt)?;
+truestmt: stmt;
+falsestmt: stmt;
+returnstmt: RETURN expr? SEMI;
+callstmt: ID LB exprlst? RB SEMI;
+forstmt:
+	FOR LB (ID ASSIGN expr) COMMA expr COMMA expr RB stmt;
+idlst: ID (COMMA ID)*;
+blockstmt: LCB stmt* RCB;
+dowhilestmt: DO blockstmt WHILE LB expr RB SEMI;
+whilestmt: WHILE LB expr RB stmt* ;
+
+functype: primitivetype;
+
+primitivetype:
+	INTEGER
+	| FLOAT
+	| STRING
+	| BOOLEAN
+	| VOID
+	| AUTO
+	| arrayType;
+
+exprlst: expr (COMMA expr)*;
+
+expr: expr1 strconcate expr1| expr1;
+expr1: expr2 relational expr2| expr2;
+expr2: expr2 binarylogical expr3 | expr3;
+expr3: expr3 adding expr4 | expr4;
+expr4: expr4 multiplying expr5 | expr5;
+expr5: unarylogical expr5 | expr6;
+expr6: sign expr6 | expr7;
+expr7: expr7 LSB exprlst RSB | expr8;
+expr8: operands | LB expr RB;
+
+adding: SUB | ADD;
+multiplying: DIV | MUL | MOD;
+relational: EQUAL | NOTEQUAL | LT | GT | LET | GET;
+strconcate: CONCATE;
+unarylogical: NOT;
+binarylogical: AND | OR;
+sign: SUB;
+operands: INTL | FLOATL | STRINGL | BOOLL | ID | arrayL | funccall;
+funccall: ID LB exprlst? RB;
+arrayType: ARRAY LSB dimension RSB OF primitivetype;
+dimension: INTL (COMMA INTL)*;
+
+// Literal
+INTL: (NoZeroNumber Number* (UNDERSCORE Number+)* | [0]) 
+	{
+		self.text = self.text.replace('_','') 
+	};
+
+FLOATL:
+	( Fragtional Exponent? | INTL+ Exponent )
+	{
+		self.text = self.text.replace('_','')
+	};
+
+BOOLL: TRUE | FALSE;
+STRINGL:
+	DoubleQuote StringChar* DoubleQuote 
+	{
+        result = str(self.text)
+        self.text = result[1:-1]
+    };
+
+arrayL: LCB exprlst RCB;
+
+// Keywords
+DO: 'do';
+FUNCTION: 'function';
+IF: 'if';
+ELSE: 'else';
+CONTINUE: 'continue';
+AUTO: 'auto';
+BREAK: 'break';
+FOR: 'for';
+RETURN: 'return';
+WHILE: 'while';
+OUT: 'out';
+OF: 'of';
+INHERIT: 'inherit';
+ARRAY: 'array';
+// Types
+VOID: 'void';
+BOOLEAN: 'boolean';
+FLOAT: 'float';
+INTEGER: 'integer';
+STRING: 'string';
+TRUE: 'true';
+FALSE: 'false';
+
+// Relational Operator 
+EQUAL: '==';
+NOTEQUAL: '!=';
+GT: '>';
+LT: '<';
+GET: '>=';
+LET: '<=';
+
+// Logical Operator
+AND: '&&';
+OR: '||';
+NOT: '!';
+
+// Arithmetic Operator
+ADD: '+';
+DIV: '/';
+SUB: '-';
+MUL: '*';
+MOD: '%';
+ASSIGN: '=';
+
+// String Operator
+CONCATE: '::';
+
+// Seperator
+LB: '(';
+RB: ')';
+LCB: '{';
+RCB: '}';
+LSB: '[';
+RSB: ']';
+COMMA: ',';
+SEMI: ';';
+DOT: '.';
+COLON: ':';
+UNDERSCORE: '_';
+
+// Fragment
+fragment Number: [0-9];
+fragment NoZeroNumber: [1-9];
+fragment Character: [a-z_A-Z];
+fragment Fragtional: INTL+ DOT INTL*;
+fragment Exponent: [Ee] [+-]? INTL+;
+fragment DoubleQuote: '"';
+fragment StringChar: ~[\b\t\f\r\n"\\] | EscapeSequence;
+fragment EscapeSequence: '\\' [bfnrt'"\\];
+fragment InvalidSequence: '\\' ~[bfrnt\\'];
+
+// Identifiers
+ID: Character (Character | Number)*;
+
+// skip spaces, tabs, newlines
+WS: [ \t\r\n\f]+ -> skip;
+
+// Comments
+LINECMT: '//' ~[\r\n]* -> skip;
+BLOCKCMT: '/*' .*? '*/' -> skip;
+
+ERROR_CHAR:
+	. {
+		raise ErrorToken(self.text)
+	};
+
+UNCLOSE_STRING:
+	DoubleQuote StringChar* ([\b\t\f\n\r"\\] | EOF) {
+        unclose_str = str(self.text)
+        possible = ['\b', '\t', '\f', '\n', '\r', '"', '\\']
+        if unclose_str[-1] in possible:
+            raise UncloseString(unclose_str[1:-1])
+        else:
+            raise UncloseString(unclose_str[1:])
+    };
+
+ILLEGAL_ESCAPE:
+	DoubleQuote StringChar* InvalidSequence {
+        illegal_str = str(self.text)
+        raise IllegalEscape(illegal_str[1:])
+    };
